78fe133b7dc92cb7d65f6dc24c441e0dc1e7c08d
chicheng567 <samuel910617@yahoo.com.tw>
2021-12-26 19:47:03 +0800

    fixTime

--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include<conio.h>
 #include<windows.h>
+#include<time.h>
 using namespace std;
 int main()
 {
+    clock_t DeltaTime;
+    clock_t totalTime;
+    clock_t lastClock;
     while(1)
     {
-        if(GetAsyncKeyState(0x41)) {
-            cout << "1" << endl;
-        }
+        clock_t TimeTMP = clock();
+        DeltaTime = TimeTMP - lastClock;
+        totalTime += DeltaTime;
+        lastClock = totalTime;
+        
     }
 }
